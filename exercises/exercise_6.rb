1475798439992dbb723b762ccca66490a6c28190
Karina Song <ksysong@gmail.com>
2021-04-27 06:28:27 +0000

    finish ex6: create one to many assoc and create employees

--- exercises/exercise_6.rb
+++ exercises/exercise_6.rb
@@ -9,3 +9,10 @@ puts "Exercise 6"
 puts "----------"
 
 # Your code goes here ...
+
+@store1.employees.create(first_name: "Khurram", last_name: "Virani", hourly_rate: 60)
+@store1.employees.create(first_name: "Kirby", last_name: "Kurbs", hourly_rate: 80)
+@store1.employees.create(first_name: "Meta", last_name: "Knight", hourly_rate: 55)
+@store2.employees.create(first_name: "Link", last_name: "Courage", hourly_rate: 30)
+@store2.employees.create(first_name: "Zelda", last_name: "Wisdom", hourly_rate: 67)
+@store2.employees.create(first_name: "Ganondorf", last_name: "Power", hourly_rate: 100)
