623bcc71a4cf1ab2dd011c76e9de258c2ef0d9c8
Karina Song <ksysong@gmail.com>
2021-04-27 06:35:22 +0000

    ex7: set up validations

--- lib/employee.rb
+++ lib/employee.rb
@@ -1,3 +1,10 @@
 class Employee < ActiveRecord::Base
   belongs_to :store
+  validates :first_name, :last_name, presence: true
+  validates :hourly_rate, inclusion: { in: 40..200 }
+  validates :store_id, presence: true
 end
+
+# employees must have first name, last name 
+# have an hourly rate of between 40 to 200
+# employees must have a store that they belong to 
--- lib/store.rb
+++ lib/store.rb
@@ -1,3 +1,8 @@
 class Store < ActiveRecord::Base
   has_many :employees
+  validates :name, length: {minimum: 3}
+  validates :annual_revenue, numericality: {only_integer: true, greater_than_or_equal_to: 0}
 end
+
+# must have a name that has a minimum of 3 characters 
+# must have an annual revenue of 0 or more 
