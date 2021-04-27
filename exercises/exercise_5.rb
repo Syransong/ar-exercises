71363b00f82b63fdb34676250b26522662c0892f
Karina Song <ksysong@gmail.com>
2021-04-27 06:23:09 +0000

    complete ex5: calculate total, avg and find count

--- exercises/exercise_5.rb
+++ exercises/exercise_5.rb
@@ -8,3 +8,15 @@ puts "Exercise 5"
 puts "----------"
 
 # Your code goes here ...
+
+# Output total revenue of all stores 
+puts Store.sum(:annual_revenue)
+# => 4114000
+
+# Output the average annual revenue for all stores 
+puts Store.average(:annual_revenue) 
+# => 822800.0
+
+# Output the number of stores generating 1M or more in annual sales 
+puts Store.where(annual_revenue: 1000000..Float::INFINITY).count
+# => 2
