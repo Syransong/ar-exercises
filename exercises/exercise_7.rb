16ef01d9cc2c2e62fe4249f3ffa40e0a0bb61c0e
Karina Song <ksysong@gmail.com>
2021-04-27 06:41:36 +0000

    finish ex 7: prints error msgs

--- exercises/exercise_7.rb
+++ exercises/exercise_7.rb
@@ -10,3 +10,9 @@ puts "Exercise 7"
 puts "----------"
 
 # Your code goes here ...
+puts "Please input a name for your store"
+
+new_store = Store.create(name: gets.chomp)
+# new_store = Store.create(name: gets.chomp, annual_revenue: 0, womens_apparel: true)
+
+puts new_store.errors.full_messages # => Annual revenue is not a number
